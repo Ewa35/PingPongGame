b40b23e9fb38440b4d4e6d39dd75c94b352de99d
Ewa Kwiecień <ewa.kwiecien.programista@gmail.com>
2022-04-10 01:30:37 +0200

    ball movement and bounce

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,6 +8,11 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+int x=-8;
+int y=-8;
+int numberOfBouncesBall=0;
+int  rightSidePoints=0;
+int leftSidePoints=0;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
@@ -55,3 +60,55 @@ void __fastcall TForm1::p2DownTimer(TObject *Sender)
         if (p2->Top+p2->Height<background->Height-5) p2->Top+=10;
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm1::ballMovementTimer(TObject *Sender)
+{
+           ball->Left+=x;
+        ball->Top+=y;
+        p2->Left=background->Width-30;
+        p1->Left=background->Left+10;
+
+
+        // odbicie od górnej   sciany
+        if (ball->Top-5<=background->Top) y=-y;
+        // odbij od Downnej sciany
+        if (ball->Top+ball->Height+5>background->Height-5)  y=-y;
+        // skucha z lewej
+        if (ball->Left<=p1->Left+p1->Width-30)
+        {
+                ballMovement->Enabled=false;
+                ball->Visible=false;
+                rightSidePoints++;
+                whoWin->Caption="Punkt dla gracza: PRAWEGO";
+                displayResult();
+        }
+
+        else if(ball->Top > p1->Top-ball->Height/2 && ball->Top < p1->Top+p1->Height &&
+                 ball->Left-ball->Width <p1->Left)
+       {
+               if (x<0) x=-x;
+               numberOfBouncesBall++;
+               ballMovement->Interval-=1;
+
+
+       }
+       // skucha z prawej
+       if (ball->Left>p2->Left+p2->Width+30)
+       {
+                ballMovement->Enabled=false;
+                ball->Visible=false;
+                leftSidePoints++;
+                whoWin->Caption="Punkt dla gracza: LEWEGO";
+                displayResult();
+        }
+        else if(ball->Top > p2->Top-ball->Height/2 && ball->Top < p2->Top+p2->Height &&
+                 ball->Left+ball->Width >p2->Left)
+       {
+               if (x>0) x=-x;
+               numberOfBouncesBall++;
+               ballMovement->Interval-=1;
+
+
+       }
+}
+//---------------------------------------------------------------------------
+
