47f87a1fd50a7a71852426c5848f914af2c29f14
Ewa Kwiecień <ewa.kwiecien.programista@gmail.com>
2022-04-11 21:34:07 +0200

    add function show information game

---
+++ Unit2.h
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TPingPong : public TForm
+{
+__published:	// IDE-managed Components
+        TMemo *Information;
+        TButton *ok;
+        void __fastcall okClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TPingPong(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TPingPong *PingPong;
+//---------------------------------------------------------------------------
+#endif
